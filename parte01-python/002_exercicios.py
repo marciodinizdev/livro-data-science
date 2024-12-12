b267dee71a5a63fa4c3d20de37480680fe6b50c4
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-12-12 20:17:39 -0300

    Create 002_exercicios.py

---
+++ parte01-python/002_exercicios.py
@@ -0,0 +1,15 @@
+# 1
+nome = 'Paulin'
+idade = 30
+print(f'Nome: {nome}. Idade: {idade}')
+
+#2
+print('\'s, \"x\", \\\\' )
+
+#3
+pi = 3.1415926
+print(f'{pi:.2f}')
+
+#4
+operacao = (5*7)-((6*4)+(20/2))
+print(operacao)
