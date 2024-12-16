13fd8d74d562d0a811c6cd5a9e157d4ce79554fe
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-12-16 16:33:57 -0300

    Update colecoes.py

--- parte01-python/colecoes.py
+++ parte01-python/colecoes.py
@@ -8,6 +8,14 @@ carros_populares.append("Uno")
 print(carros_populares[3], " foi adicionado!")
 print("Lista de carros populares atualizada: ", carros_populares)
 
+carros_tunados.append("Viper")
+print(carros_tunados[3], " foi adicionado")
+print("Lista de carros tunados atualizada: ", carros_tunados)
+
 print(carros_populares[1], " foi removido!")
 carros_populares.pop(1)
 print("Lista de carros populares atualizada: ", carros_populares)
+
+print(carros_tunados[1], " foi removido")
+carros_tunados.pop(1)
+print("Lista de carros tunados atualizada: ", carros_tunados)
