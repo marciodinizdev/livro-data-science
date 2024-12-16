4099350c299d58c8e440ee431c103e08b5f7f16d
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-12-16 16:47:10 -0300

    Update colecoes.py

--- parte01-python/colecoes.py
+++ parte01-python/colecoes.py
@@ -1,25 +1,27 @@
+# Listas
 carros_populares = ["Corsa", "HB20", "Ka"]
 print("Carros populares: ", carros_populares)
 
 carros_tunados = ["Ferrari", "Porshe", "Supra"]
 print("Carros tunados: ", carros_tunados)
 
-# carros_populares.append("Uno")
-# print(carros_populares[3], " foi adicionado!")
-# print("Lista de carros populares atualizada: ", carros_populares)
+carros_populares.append("Uno")
+print(carros_populares[3], " foi adicionado!")
+print("Lista de carros populares atualizada: ", carros_populares)
 
-# carros_tunados.append("Viper")
-# print(carros_tunados[3], " foi adicionado")
-# print("Lista de carros tunados atualizada: ", carros_tunados)
+carros_tunados.append("Viper")
+print(carros_tunados[3], " foi adicionado")
+print("Lista de carros tunados atualizada: ", carros_tunados)
 
-# print(carros_populares[1], " foi removido!")
-# carros_populares.pop(1)
-# print("Lista de carros populares atualizada: ", carros_populares)
+print(carros_populares[1], " foi removido!")
+carros_populares.pop(1)
+print("Lista de carros populares atualizada: ", carros_populares)
 
-# print(carros_tunados[1], " foi removido")
-# carros_tunados.pop(1)
-# print("Lista de carros tunados atualizada: ", carros_tunados)
+print(carros_tunados[1], " foi removido")
+carros_tunados.pop(1)
+print("Lista de carros tunados atualizada: ", carros_tunados)
 
+#Listas de listas
 carros = [carros_populares, carros_tunados]
 print(f'Lista de carros completa: {carros}')
 print(f'Carro popular: {carros[0][0]}')
