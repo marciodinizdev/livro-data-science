e1cb4fa97a765f9977a9dd6f30ec5d92c04aead8
Márcio Diniz <marciodiniz.dev@gmail.com>
2024-12-30 21:39:02 -0300

    Update 022_loop-aninhado.py

--- Parte-01_Python/Cap-04_Estruturas-de-Repeticao/022_loop-aninhado.py
+++ Parte-01_Python/Cap-04_Estruturas-de-Repeticao/022_loop-aninhado.py
@@ -8,3 +8,25 @@ letra = 0
 for numero in range(3):
     for letra in range(3):
         print(f'As salas são: {numeros[numero]}{letras[letra]}')
+
+# Loop aninhado para testar todas as magias resultantes de combinações elementais de um jogo incluindo condicionais
+elementos_primarios = ['Fogo', 'Água']
+elementos_secundarios = ['Terra', 'Vento']
+
+elemento_1 = 0
+elemento_2 = 0
+
+for elemento_1 in range(2):
+    for elemento_2 in range(2):
+        print(f'Você combinou os elementos {elementos_primarios[elemento_1]} e {elementos_secundarios[elemento_2]}')
+
+# O código acima pode ser reescrito como
+elementos_primarios = ['Fogo', 'Água']
+elementos_secundarios = ['Terra', 'Vento']
+
+elemento_1 = 0
+elemento_2 = 0
+
+for elemento_1 in elementos_primarios:
+    for elemento_2 in elementos_secundarios:
+        print(f'Você combinou os elementos {elemento_1} e {elemento_2}')
