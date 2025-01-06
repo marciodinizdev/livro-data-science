ffdf06c8ac3ebfd6874c8a51d7f4bf54f896991b
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-06 20:44:53 -0300

    Create 024_compreensaoes-de-listas.py

---
+++ Parte-01_Python/Cap-04_Estruturas-de-Repeticao/024_compreensaoes-de-listas.py
@@ -0,0 +1,18 @@
+# Especificando precisamente listas em conjuntos algébricos
+
+print('Entendendo List Comprehensions:')
+
+# Imprimindo uma lista c1 formada por um conjunto de números de 0 até 3 elevados ao cubo
+print('C1 = {x³ | 0<=x<=3}')
+c1 = [x**3 for x in range(4)]
+print(c1)
+
+# Imprimindo uma lista c2 formada por um conjunto de números de 0 até 20 sendo todos pares
+print('x | 0<=x<=20 e x é par')
+c2 = [x * 2 for x in range(0, 11)] # produz o mesmo que [x for x in range(0, 21, 2)]
+print(c2)
+
+# Imprimindo uma lista c2 formada por um conjunto de números de -20 até 40 sendo todos ímpares
+print('x | -20<=x<=40 e x é ímpar')
+c3 = [x * 2 + 1 for x in range(-10, 20)]
+print(c3, sep='')
