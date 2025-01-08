542e11c1f4823d0b7f73b36e23d2b1ea37804067
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-07 22:17:57 -0300

    Update 026_exercicios-propostos.py

--- Parte-01_Python/Cap-04_Estruturas-de-Repeticao/026_exercicios-propostos.py
+++ Parte-01_Python/Cap-04_Estruturas-de-Repeticao/026_exercicios-propostos.py
@@ -10,3 +10,19 @@ for x in range(2000, 5000, 2):
     if(x % 4 == 0 and x % 3 == 0):
         print(x * 1.5, end=' ')
 
+#3
+print('Crivo de Eratóstenes para encontrar números primos entre 0 e 100:')
+import math
+
+numeros = []
+for i in range(101):
+    numeros.append(True)
+i = 2
+while(i <= int(math.sqrt(100))):
+    if numeros[i]:
+        for j in range(i*2, 101, i):
+            numeros[j]=False
+    i = i+1
+for i in range(2,101):
+  if numeros[i]:
+      print(i)
