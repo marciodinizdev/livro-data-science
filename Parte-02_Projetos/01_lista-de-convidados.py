2ed3107180421490e674488b73931db61f863793
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-01-13 11:33:42 -0300

    Create 01_lista-de-convidados.py

---
+++ Parte-02_Projetos/01_lista-de-convidados.py
@@ -0,0 +1,22 @@
+'''
+Programa que recebe nomes de convidados para uma festa e os armazena em uma lista. Ao final, exibe:
+
+1 -> Quantos convidados foram adicionados.
+2 -> A lista completa de convidados.
+3 -> Uma mensagem personalizada para cada convidado, como: "Bem-vindo, [nome]! Estamos ansiosos para a festa!".
+
+'''
+convidados = []
+while True:
+    nome = input('Digite o nome do convidado ou "sair" para encerrar: ')
+    if nome.lower() == 'sair':
+        break
+    convidados.append(nome)
+    print(f'{nome} foi adicionado à lista de convidados.\n')
+
+print(f'Foram adicionados {len(convidados)} convidados à lista. \n')
+print(f'Lista de convidados: {convidados}\n')
+
+print('Mensagens para os convidados:\n')
+for convidado in convidados:
+    print(f'Bem-vindo, {convidado}! Estamos ansiosos para a festa!\n')
